5037d00170cd767669372a153d51a8aca36e1cc2
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2023-09-07 18:11:05 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 476 ms, Memory: 124156 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,24 +1,21 @@
-r= 1000000
-
-check = [True for _ in range(r)]
-
-for i in range(2,int(r**0.6)):
-    if check[i]==True:
-        for j in range(i*2, r, i) : 
-            if check[j] == True :
-                check[j] = False          
-
-
 import sys
 input = sys.stdin.readline
 
-
-while(True):                               
-    n = int(input())
-
-    if n==0 : break
-    for i in range(3,r):
-        if check[i] == True:
-            if check[n-i] == True :
-                print("%d = %d + %d"%(n , i , n-i))
-                break
+n = 1000001
+# 소수 아닌 모든 홀수 처리
+table = [1] * n
+for i in range(3, int(n ** 0.5) + 1, 2):
+    if table[i]:
+        for j in range(i * 2, n, i):
+            table[j] = 0
+# k가 만들어지는 두 홀수인 소수 검색
+while 1:
+    k = int(input())
+    if not k:
+        break
+    for i in range(3, int(k / 2) + 1, 2):
+        if table[i] and table[k - i]:
+            print(f"{k} = {i} + {k - i}")
+            break
+    else:
+        print("Goldbach's conjecture is wrong.")
