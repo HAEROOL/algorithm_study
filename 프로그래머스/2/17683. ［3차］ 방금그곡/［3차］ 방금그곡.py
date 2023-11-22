d7bf8f4a00b8741e180e3cc44ac7d762695d8c59
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2023-11-22 15:45:00 +0900

    [level 2] Title: [3차] 방금그곡, Time: 1.30 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -0,0 +1,18 @@
+def solution(m, musicinfos):
+    m = m.replace('C#','c').replace('D#','d').replace('F#','f').replace('A#','a').replace('G#','g')
+    found = {'time': 0, 'name': '(None)'}
+
+    for data in musicinfos:
+        start, end, name, song = data.split(',')
+
+        start_hour, start_min = map(int, start.split(':'))
+        end_hour, end_min = map(int, end.split(':'))
+        length = (end_hour-start_hour)*60 + (end_min-start_min)
+
+        song = song.replace('C#','c').replace('D#','d').replace('F#','f').replace('A#','a').replace('G#','g')
+        song = (song*max(1, int(length/len(song))+1))[:length]
+
+        if m in song and found['time'] < length:
+            found = {'time': length, 'name': name}
+
+    return found['name']
