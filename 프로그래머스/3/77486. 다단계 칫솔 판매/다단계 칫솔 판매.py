ed60288d8eb256baf01e5428f4a19842d957963d
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2023-12-25 20:04:20 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 213.14 ms, Memory: 20.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
@@ -0,0 +1,26 @@
+def solution(enroll, referral, seller, amount):
+    result = []
+    memberRecommender = dict()
+    memberProfit = dict()
+
+    for i in range(len(enroll)):
+        memberRecommender[enroll[i]] = referral[i]
+        memberProfit[enroll[i]] = 0
+
+    for i in range(len(seller)):
+        cur = seller[i]
+        profit = amount[i]*100
+        memberProfit[cur] += amount[i]*100
+        while memberRecommender[cur] != '-':
+            if profit == 0:
+                break
+            profit = int(0.1*profit)
+            memberProfit[cur] -= profit
+            memberProfit[memberRecommender[cur]] += profit
+            cur = memberRecommender[cur]    
+        if memberRecommender[cur] == '-':
+            memberProfit[cur] -= int(profit*0.1)
+    for member in enroll:
+        result.append(memberProfit[member])
+    return result
+
