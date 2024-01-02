79d887b7c5088414b1b4480921692715add79d17
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-01-02 19:21:21 +0900

    [Silver I] Title: 카드 구매하기, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -1,10 +1,13 @@
 n = int(input())
 
-p = [0] + list(map(int, input().split()))
-dp = [0 for _ in range(n+1)]
+arr = list(map(int, input().split()))
 
-for i in range(1, n+1):
-    for j in range(1, i+1):
-        dp[i] = max(dp[i], dp[i-j] + p[j])
+
+dp = [0] * (n + 1)
+
+dp[1] = arr[0]
+
+for x in range(1, n + 1):
+    for y in range(1, x + 1):
+        dp[x] = max(dp[x], dp[x - y] + arr[y - 1])
 print(dp[n])
-
