310917aa1ba3bb8e1f2fcbc2c3b94b40b2fc6e44
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-01-09 18:38:13 +0900

    [Silver I] Title: 1로 만들기 2, Time: 912 ms, Memory: 414532 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -1,17 +1,22 @@
-N = int(input())
+n = int(input())
 
-dp = [[1] for _ in range(0, N + 1)]
+dp = [[0]] * (n + 2)
 
 dp[1] = [1]
+dp[2] = [2, 1]
+if n < 3:
+    print(len(dp[n]) - 1)
+    print(*dp[n])
+else:
+    dp[3] = [3, 1]
 
-for x in range(2, N + 1):
-    dp[x] = [x] + dp[x - 1]
-    if x % 3 == 0:
-        if len(dp[x]) > len([x] + dp[x // 3]):
+    for x in range(4, n + 2):
+        dp[x] = [x] + dp[x - 1]
+        if x % 3 == 0 and len(dp[x]) > len([x] + dp[x // 3]):
             dp[x] = [x] + dp[x // 3]
-    if x % 2 == 0:
-        if len(dp[x]) > len([x] + dp[x // 2]):
+        if x % 2 == 0 and len(dp[x]) > len([x] + dp[x // 2]):
             dp[x] = [x] + dp[x // 2]
-        
-print(len(dp[N]) - 1)
-print(*dp[N])
+
+
+    print(len(dp[n]) - 1)
+    print(*dp[n])
