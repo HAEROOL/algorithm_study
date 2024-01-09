76ef97fc8ad10e814a4b7bc5f0be0eb4fd1b38db
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-01-09 19:33:28 +0900

    [Silver I] Title: 정수 삼각형, Time: 156 ms, Memory: 111364 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -3,12 +3,12 @@ d=[]
 for i in range(n):
   d.append(list(map(int, input().split())))
 
-for i in range(1,n):
-  for j in range(len(d[i])):
-    if j==0:
-      d[i][j]=d[i][j]+d[i-1][j]
-    elif j==len(d[i])-1: 
-      d[i][j]=d[i][j]+d[i-1][j-1]
+for x in range(1, n):
+  for y in range(0, len(d[x])):
+    if y == 0:
+      d[x][y] = d[x][y] + d[x - 1][y]
+    elif y == len(d[x]) - 1:
+      d[x][y] = d[x][y] + d[x - 1][y - 1]
     else:
-      d[i][j]=max(d[i-1][j-1],d[i-1][j])+d[i][j]
+      d[x][y] = max(d[x - 1][y], d[x - 1][y - 1]) + d[x][y]
 print(max(d[n-1]))
