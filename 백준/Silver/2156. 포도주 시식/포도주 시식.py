f033c83e5fb414d39c416d7836805368a398d5e5
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-01-16 20:22:07 +0900

    [Silver I] Title: 포도주 시식, Time: 152 ms, Memory: 111732 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,16 +1,14 @@
 n = int(input())
-wines = []
-
-for _ in range(n):
-    wines.append(int(input()))
+nums = []
+for x in range(1, n + 1):
+    nums.append(int(input()))
 
 dp = [0] * (n + 1)
-
-dp[1] = wines[0]
+dp[1] = nums[0]
 if n > 1: 
-    dp[2] = wines[1] + dp[1]
+    dp[2] = nums[1] + dp[1]
 
 
 for x in range(3, n + 1):
-    dp[x] = max(dp[x - 1], dp[x - 2] + wines[x - 1], wines[x - 1] + wines[x - 2] + dp[x - 3])
+    dp[x] = max(dp[x - 2] + nums[x - 1], nums[x - 1] + nums[x - 2] + dp[x - 3], dp[x - 1])
 print(max(dp))
