301d859aa020c43317dfbb23833607fa2528434d
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-02-21 19:24:41 +0900

    [Gold V] Title: 적록색약, Time: 92 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -10,9 +10,8 @@ def BFS(x,y):
         for d in range(4):
             nx = x + dx[d]
             ny = y + dy[d]
-            # 인덱스 범위 안에 있으면서, 같은 색이면서, 방문 안한 경우
             if 0<=nx<N and 0<=ny<N and a[nx][ny] == a[x][y] and not visited[nx][ny]:
-                visited[nx][ny] = 1  # 방문체크 후 큐에 넣음
+                visited[nx][ny] = 1
                 q.append((nx,ny))
 
 
@@ -20,16 +19,14 @@ N = int(input())
 a = [list(input()) for _ in range(N)]
 q = deque()
 
-# 적록색약 아닌 경우
 visited = [[0] * N for _ in range(N)]
 cnt1 = 0
 for i in range(N):
     for j in range(N):
-        if not visited[i][j]:  # 아직 방문 안한 경우만 체킹
+        if not visited[i][j]:
             BFS(i,j)
             cnt1 += 1
 
-# 적록색약인 경우
 for i in range(N):
     for j in range(N):
         if a[i][j] == 'G':
@@ -43,4 +40,4 @@ for i in range(N):
             BFS(i,j)
             cnt2 += 1
 
-print(cnt1, cnt2)
+print(cnt1, cnt2) 
