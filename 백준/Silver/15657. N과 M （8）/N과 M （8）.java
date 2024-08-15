10bedaddcb04d36909ab0ca8adee9883f5c9278d
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-08-15 21:35:29 +0900

    [Silver III] Title: N과 M (8), Time: 232 ms, Memory: 24712 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  static int[] arr;
+  static void recursive(int idx, int k, int[] sel) throws IOException {
+    if(idx == sel.length){
+      for(int i : sel){
+        bw.write(arr[i] + " ");
+      }
+      bw.write("\n");
+      return;
+    }
+    for(int i = k ; i < N ; i++){
+        sel[idx] = i;
+        recursive(idx + 1, i, sel);
+    }
+  }
+  static int N, M;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    N = input[0];
+    M = input[1];
+    arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    Arrays.sort(arr);
+    // N 개중 M개 고르기
+    recursive(0,  0, new int[M]);
+    bw.flush();
+    bw.close();
+  }
+}
