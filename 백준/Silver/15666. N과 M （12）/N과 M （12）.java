02d72baf442f21a8fe9a1db5e4b8723f8296dddb
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-08-16 22:45:01 +0900

    [Silver II] Title: N과 M (12), Time: 200 ms, Memory: 21480 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.java
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -1,45 +1,40 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  static int N, M;
   static int[] arr;
-  static public void recursive(int k, int idx, int[] sel) throws IOException {
-    if(idx == M){
+  static void recursive(int idx, int k, int[] sel) throws IOException {
+    if(idx == sel.length){
       for(int i : sel){
         bw.write(arr[i] + " ");
       }
       bw.write("\n");
       return;
     }
-    if(k > N) return;
     int tmp = 0;
     for(int i = k ; i < N ; i++){
-        if(tmp != arr[i]) {
-          sel[idx] = i;
-          tmp = arr[i];
-
-          recursive(i, idx + 1, sel);
-
-        }
+      if(tmp != arr[i]){
+        sel[idx] = i;
+        tmp = arr[i];
+        recursive(idx + 1, i, sel);
       }
+
+
     }
 
-  static int N, M;
+  }
+
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-    arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
     N = input[0];
     M = input[1];
+    arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
     Arrays.sort(arr);
-    // N 개중 M개 고르기
-    recursive(0, 0, new int[M]);
+    // N개중 M개 고르기
+    recursive(0,  0, new int[M]);
     bw.flush();
     bw.close();
   }
