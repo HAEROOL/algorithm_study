b595325be6e8b67f5383d6c5acb246b63286990d
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-08-23 00:30:54 +0900

    [Gold II] Title: 빵집, Time: 324 ms, Memory: 52520 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -15,7 +15,7 @@ import java.util.*;
 public class Main {
 	static int[] dx = {-1, 0, 1};
 	static int[] dy = {1, 1, 1};
-	static String[][] map;
+	static char[][] map;
 	static int cnt = 0;
 	static boolean[][] visited;
 	static int R, C;
@@ -28,12 +28,12 @@ public class Main {
 		for(int i = 0 ; i < 3 ; i++) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
-			if(0 <= nx && nx < R && 0 <= ny && ny < C && !visited[nx][ny] && map[nx][ny].equals(".")) {
+			if(0 <= nx && nx < R && 0 <= ny && ny < C && !visited[nx][ny] && map[nx][ny] == '.') {
 				if(dfs(nx, ny)) {
 					visited[nx][ny] = true;
 					return true;
 				}else {
-					map[nx][ny] = "x";
+					map[nx][ny] = 'x';
 				}
 			}
 		}
@@ -47,13 +47,12 @@ public class Main {
 		// 입력 및 초기화
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-		map = new String[R][C];
+		map = new char[R][C];
 		visited = new boolean[R][C];
 		for(int i = 0 ; i < R ; i++) {
-			String[] arr = br.readLine().split("");
-			for(int j = 0 ; j < C ; j++) {
-				map[i][j] = arr[j];
-			}
+			st = new StringTokenizer(br.readLine());
+			String temp_str = st.nextToken();
+			map[i] = temp_str.toCharArray();
 		}
 		
 		for(int i = 0 ; i < R ; i++) {
