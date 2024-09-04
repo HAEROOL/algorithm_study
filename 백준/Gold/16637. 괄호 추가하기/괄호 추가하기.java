fa40a23c52c39cdda9648416a6cd1a52abd222aa
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-09-04 19:43:06 +0900

    [Gold III] Title: 괄호 추가하기, Time: 64 ms, Memory: 11804 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
@@ -1,75 +1,56 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-  static int N;
-  static List<String> nums;
-  static int mx = Integer.MIN_VALUE;
-  static void combination(int idx, int k, boolean[] sel){
-    if(idx >= sel.length){
-      run(sel);
-      return;
-    }
-
-    sel[idx] = true;
-    combination(idx + 2, k + 1, sel);
-    sel[idx] = false;
-    combination(idx + 1, k, sel);
-  }
-
-  static int cal(int a, String b, int c){
-    int result = 0;
-    switch (b){
-      case "+":
-        result = a + c;
-        break;
-      case "-":
-        result = a - c;
-        break;
-      case "*":
-        result = a * c;
-        break;
-    }
-    return result;
-  }
-
-  static void run(boolean[] sel){
-    List<String> list = new ArrayList<>();
-    int total = 0;
-    int cnt = 0;
-    for(int i = 0 ; i < nums.size() ; i++){
-      if(nums.get(i).equals("*") || nums.get(i).equals("+") || nums.get(i).equals("-")){
-//        System.out.println(Arrays.toString(sel));
-        if(sel[cnt]){
-          list.remove(list.size() - 1);
-          int a = Integer.parseInt(nums.get(i - 1));
-          int c = Integer.parseInt(nums.get(i + 1));
-          int result = cal(a, nums.get(i), c);
-          list.add(String.valueOf(result));
-          i++;
-        }else{
-          list.add(nums.get(i));
-        }
-        cnt++;
-      }else{
-        list.add(nums.get(i));
-      }
-    }
-    total = Integer.parseInt(list.get(0));
-    for(int i = 1 ; i < list.size() - 1 ; i+=2){
-      total = cal(total, list.get(i), Integer.parseInt(list.get(i + 1)));
-    }
-    mx = Math.max(total, mx);
-  }
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    N = Integer.parseInt(br.readLine());
-    nums = Arrays.asList(br.readLine().split(""));
-    combination(0, 0, new boolean[N]);
-    System.out.println(mx);
-  }
+	static List<Integer> nums = new ArrayList<Integer>();
+	static List<String> ops = new ArrayList<String>();
+	static void run(int idx, int total) {
+		if(idx >= ops.size()) {
+			ans = Math.max(ans, total);
+			return;
+		}
+		run(idx + 1, cal(ops.get(idx), total, nums.get(idx + 1)));
+		if(idx + 1 < ops.size()) {
+			int tmp = cal(ops.get(idx + 1), nums.get(idx + 1), nums.get(idx + 2));
+			int res = cal(ops.get(idx), total, tmp);
+			run(idx + 2, res);
+		}
+		
+	}
+	static int cal(String op, int a, int b) {
+		if(op.equals("*")) {
+			return a * b;
+		}
+		if(op.equals("+")) {
+			return a + b;
+		}
+		if(op.equals("-")) {
+			return a - b;
+		}
+		return 1;	
+	}
+	static int ans = Integer.MIN_VALUE;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		String[] input = br.readLine().split("");
+		for(int i = 0 ; i < N ; i++) {
+			if(i % 2 == 0) {
+				nums.add(Integer.parseInt(input[i]));
+			}else {
+				ops.add(input[i]);
+			}
+		}
+		run(0, nums.get(0));
+		bw.write(ans + "");
+		bw.flush();
+	}
 }
