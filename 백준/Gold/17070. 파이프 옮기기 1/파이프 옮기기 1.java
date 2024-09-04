b81a1956b66789d7dc643cf7a3cd6d43274e5258
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-09-04 23:34:40 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 644 ms, Memory: 294988 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,95 +1,98 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-  static int N;
-  static int[][] board;
-  static int cnt = 0;
-  static class Pipe{
-    int x1, y1;
-    int x2, y2;
-    // 0: 가로, 1: 대각, 2: 세로
-    int dir;
-    Pipe(){
-      x1 = 0;
-      y1 = 0;
-      x2 = 0;
-      y2 = 1;
-      dir = 0;
-    }
-  }
-  static void dfs(int x, int y, int dir, boolean[][] visited){
-    if(dir == 1 && (board[x - 1][y] == 1 || board[x][y - 1] == 1)){
-      return;
-    }
-    if(x == N - 1 && y == N - 1){
-      cnt++;
-      return;
-    }
-    //현재 방향 가로면
-    if(dir == 0){
-      // x1, x2
-      int[] dx = {0, 1};
-      // y1, y2
-      int[] dy = {1, 1};
-      for(int i = 0 ; i < 2 ; i ++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(0 <= nx && 0 <= ny && nx < N && ny < N && !visited[nx][ny] && board[nx][ny] != 1){
-          visited[nx][ny] = true;
-          dfs(nx, ny, i, visited);
-          visited[nx][ny] = false;
-        }
-      }
-    }
-    else if(dir == 1){
-      // x1, x2
-      int[] dx = {0, 1, 1};
-      // y1, y2
-      int[] dy = {1, 1, 0};
-      for(int i = 0 ; i < 3 ; i ++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(0 <= nx && 0 <= ny && nx < N && ny < N && !visited[nx][ny] && board[nx][ny] != 1){
-          visited[nx][ny] = true;
-          dfs(nx, ny, i, visited);
-          visited[nx][ny] = false;
-        }
-      }
-    }
-    else if(dir == 2){
-      // x1, x2
-      int[] dx = {1, 1};
-      // y1, y2
-      int[] dy = {1, 0};
-      for(int i = 2 ; i > 0 ; i --){
-        int nx = x + dx[i - 1];
-        int ny = y + dy[i - 1];
-        if(0 <= nx && 0 <= ny && nx < N && ny < N && !visited[nx][ny] && board[nx][ny] != 1){
-          visited[nx][ny] = true;
-          dfs(nx, ny, i, visited);
-          visited[nx][ny] = false;
-        }
-      }
-    }
-  }
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    N = Integer.parseInt(br.readLine());
-    Pipe p = new Pipe();
-    board = new int[N][N];
-    for(int x = 0 ; x < N ; x++){
-      int[] row = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-      board[x] = row;
-    }
-    boolean[][] visited = new boolean[N][N];
-    visited[0][1] = true;
-//    visited[0][0] = true;
-    dfs(0, 1, 0, visited);
-    System.out.println(cnt);
-  }
+	static int[][] map;
+	static int N;
+	static boolean[][] visited;
+	static int ans = 0;
+	static void dfs(int sx, int sy, int state, List<Integer> log) {
+		if(state == 1 && (map[sx - 1][sy] != 0 || map[sx][sy - 1] != 0)) {
+			return;
+		}
+		if (sx == N - 1 && sy == N - 1) {
+			ans++;
+//			System.out.println(log.toString());
+			return;
+		}
+		// 가로일때
+		if (state == 0) {
+			int[] dx = {0, 1};
+			int[] dy = {1, 1};
+			for(int i = 0 ; i < 2 ; i++) {
+				int nx = sx + dx[i];
+				int ny = sy + dy[i];
+				if(0 <= nx && nx < N && 0 <= ny && ny < N) {
+					if(!visited[nx][ny] && map[nx][ny] == 0) {
+						visited[nx][ny] = true;
+						log.add(i);
+						dfs(nx, ny, i, log);
+						visited[nx][ny] = false;
+						log.remove(0);
+					}
+				}
+			}
+		}
+		// 대각선 일때
+		else if (state == 1) {
+			int[] dx = {0, 1, 1};
+			int[] dy = {1, 1, 0};
+			for(int i = 0 ; i < 3 ; i++) {
+				int nx = sx + dx[i];
+				int ny = sy + dy[i];
+				if(0 <= nx && nx < N && 0 <= ny && ny < N) {
+					if(!visited[nx][ny] && map[nx][ny] == 0) {
+						log.add(i);
+						dfs(nx, ny, i, log);
+						visited[nx][ny] = false;
+						log.remove(0);
+					}
+				}
+			}
+		}
+		// 세로일때
+		else if (state == 2) {
+			int[] dx = {1, 1};
+			int[] dy = {1, 0};
+			for(int i = 1 ; i < 3 ; i++) {
+				int nx = sx + dx[i - 1];
+				int ny = sy + dy[i - 1];
+				if(0 <= nx && nx < N && 0 <= ny && ny < N) {
+					if(!visited[nx][ny] && map[nx][ny] == 0) {
+						log.add(i);
+						dfs(nx, ny, i, log);
+						visited[nx][ny] = false;
+						log.remove(0);
+					}
+				}
+			}
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		visited = new boolean[N][N];
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		dfs(0, 1, 0, new ArrayList<Integer>());
+		bw.write(ans + "");
+		bw.close();
+	}
 }
