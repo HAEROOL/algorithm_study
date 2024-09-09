4b578dc518337da87580c8361980ca950ae3c463
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-09-09 15:51:57 +0900

    [Gold II] Title: 색종이 붙이기, Time: 184 ms, Memory: 21056 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -3,75 +3,78 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-  static int[][] map;
-  static int[] papers = {0, 5, 5, 5, 5, 5};
-  static int ans = Integer.MAX_VALUE;
+	static int ans = Integer.MAX_VALUE;
+	static int[] paper = {0, 5, 5, 5, 5, 5};
+	static int[][] map;
+	static void backtracking(int x, int y, int cnt) {
+//		System.out.println(cnt);
+		if(x >= 9 && y > 9) {
+//			System.out.println(cnt);
+			ans = Math.min(ans, cnt);
+			return;
+		}
+		if(ans <= cnt) {
+			return;
+		}
+		if(y > 9) {
+			backtracking(x + 1, 0, cnt);
+			return;
+		}
+		if(map[x][y] == 1) {
+			for(int i = 5 ; i > 0 ; i--) {
+//				System.out.println(check(x, y, i));
+				if(paper[i] > 0 && check(x, y, i)) {
+					marking(x, y, i, 0);
+					paper[i]--;
+					backtracking(x, y + 1, cnt + 1);
+					marking(x, y, i, 1);
+					paper[i]++;					
+				}
+			}
+		}else {
+			backtracking(x, y + 1, cnt);
+		}
+	}
+	static boolean check(int x, int y, int size) {
+	    for(int i = x ; i <  x + size ; i++){
+	        for(int j = y ; j < y + size ; j++){
+	          if(i < 0 || i >= 10 || j < 0 || j>= 10) return false;
+	          if(map[i][j] != 1) return false;
+	        }
+	      }
+	      return true;
+	}
+	static void marking(int x, int y, int k, int state) {
+		for(int i = x ; i < x + k ; i++) {
+			for(int j = y ; j < y + k ; j++) {
+				map[i][j] = state;
+			}
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-  static void dfs(int x, int y, int cnt){
-    if(x >= 9 && y > 9){
-      ans = Math.min(ans, cnt);
-      return;
-    }
+		StringTokenizer st;
+		map = new int[10][10];
 
-    if(ans <= cnt){
-      return;
-    }
+		for (int i = 0; i < 10; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 10; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-    if(y > 9){
-      dfs(x + 1, 0, cnt);
-      return;
-    }
-    if(map[x][y] == 1){
-      for(int i = 5 ; i > 0; i--){
-        if(papers[i] > 0 && isAttatch(x, y, i)){
-          attatch(x, y, i, 0);
-          papers[i]--;
-          dfs(x, y + 1, cnt + 1);
-          attatch(x, y, i, 1);
-          papers[i]++;
-        }
-      }
-    }else{
-      dfs(x, y + 1, cnt);
-    }
-  }
-
-  static boolean isAttatch(int x, int y , int size){
-    for(int i = x ; i <  x + size ; i++){
-      for(int j = y ; j < y + size ; j++){
-        if(i < 0 || i >= 10 || j < 0 || j>= 10) return false;
-        if(map[i][j] != 1) return false;
-      }
-    }
-    return true;
-  }
-
-  static void attatch(int x, int y, int size, int state){
-    for(int i = x ; i <  x + size ; i++){
-      for(int j = y ; j < y + size ; j++){
-        map[i][j] = state;
-      }
-    }
-  }
-
-  public static void main(String[] args) throws IOException {
-    // TODO Auto-generated method stub
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    map = new int[10][10];
-    for(int i = 0 ; i < map.length ; i++){
-      int[] row = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-      map[i] = row;
-    }
-
-    dfs(0, 0, 0);
-    bw.write(ans == Integer.MAX_VALUE ? -1 + "" : ans + "");
-    bw.flush();
-    bw.close();
-  }
-
+		backtracking(0, 0, 0);
+		ans = ans == Integer.MAX_VALUE?-1:ans;
+		bw.write(ans + "");
+		bw.close();
+	}
 }
