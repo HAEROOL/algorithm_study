b9c82a8c81aea846423af571cfbb1e7e4b02b876
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-10-20 23:17:06 +0900

    [Silver IV] Title: 행운의 바퀴, Time: 64 ms, Memory: 11692 KB -BaekjoonHub

---
+++ 백준/Silver/2840. 행운의 바퀴/행운의 바퀴.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws IOException {
+		   BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		   StringTokenizer st = new StringTokenizer(in.readLine());
+		   int N = Integer.parseInt(st.nextToken());
+		   int K = Integer.parseInt(st.nextToken());
+		 
+		   boolean[] alphabet = new boolean[26];
+		 
+		   char[] roulette = new char[N];
+		   Arrays.fill(roulette, '?');
+		 
+		   int idx = 0;
+		   while (--K >= 0) {
+		      st = new StringTokenizer(in.readLine());
+		      idx = (idx + N - (Integer.parseInt(st.nextToken()) % N)) % N;
+		      char ch = st.nextToken().charAt(0);
+		 
+		      if (roulette[idx] == ch) continue;
+		 
+		      if (roulette[idx] == '?' && !alphabet[ch-'A']) {
+		         roulette[idx] = ch;
+		         alphabet[ch-'A'] = true;
+		      }
+		      else {
+		         System.out.println('!');
+		         return;
+		      }
+		   }
+		 
+		   StringBuilder sb = new StringBuilder();
+		   for (int i = 0; i < N; i++) {
+		      sb.append(roulette[(idx + i) % N]);
+		   }
+		   System.out.println(sb);
+		}
+}
