f9e300809a5348f624a9e7147932619eb1768a9f
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-10-24 16:19:00 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 357 ms, Memory: 97,896 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -1,121 +1,112 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-import java.util.concurrent.PriorityBlockingQueue;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
-	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = { 0, 0, -1, 1 };
-	static int N;
-	static Cell[] cells;
-	static class Cell {
-		int x, y, size, d, idx, mxSize;
-
-		public Cell(int x, int y, int size, int d, int idx) {
-			// TODO Auto-generated constructor stub
-			this.x = x;
-			this.y = y;
-			this.size = size;
-			this.mxSize = size;
-			this.d = d;
-			this.idx = idx;
-		}
-
-		int[] move() {
-			x += dx[this.d];
-			y += dy[this.d];
-			if (x == 0 || x == N - 1 || y == 0 || y == N - 1) {
-				this.size /= 2;
-				if(this.size == 0) {
-					cells[idx] = null;
-					return null;
-				}
-				if (this.d == 1) {
-					this.d = 0;
-				} else if (this.d == 0) {
-					this.d = 1;
-				} else if (this.d == 2) {
-					this.d = 3;
-				} else if (this.d == 3) {
-					this.d = 2;
-				}
-			}
-			return new int[] { x, y };
-		}
-		void update(Cell[][] board) {
-			int[] coord = move();
-			if(coord == null) return;
-			if(board[coord[0]][coord[1]] != null) {
-				if(this.mxSize < board[coord[0]][coord[1]].mxSize) {
-					board[coord[0]][coord[1]].size += this.size;
-					cells[this.idx] = null;
-				}else{
-					this.size += board[coord[0]][coord[1]].size;
-					cells[board[coord[0]][coord[1]].idx] = null;
-					board[coord[0]][coord[1]] = this;
-				}
-			}else {
-				board[coord[0]][coord[1]] = this;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int[][] map;
+	static void copyBoard(int[][] tmp) {
+		for(int i = 0 ; i < map.length ; i++) {
+			for(int j = 0 ; j < map[i].length ; j++) {
+				tmp[i][j] = map[i][j];
 			}
 		}
 	}
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int T = Integer.parseInt(br.readLine());
-
-		for (int tc = 1; tc < T + 1; tc++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			int K = Integer.parseInt(st.nextToken());
-			cells = new Cell[K];
-			Cell[][] map = new Cell[N][N];
-			for(int i = 0 ; i < K ; i++) {
-				st = new StringTokenizer(br.readLine());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				int size = Integer.parseInt(st.nextToken());
-				int dir = Integer.parseInt(st.nextToken()) - 1;
-				cells[i] = new Cell(x, y, size, dir, i);
-				map[x][y] = new Cell(x, y, size, dir, i);
-			}
-			for(int time = 0 ; time < M ; time++) {
-				Cell[][] tmp = new Cell[N][N];
-				for(Cell cell : cells) {
-					if(cell == null) continue;
-					cell.update(tmp);
-				}
-				map = tmp;
-				for(Cell[] row : tmp) {
-					for(Cell c : row) {
-						if(c == null) continue;
-						c.mxSize = c.size;
+	static class Cell {
+		int id, val, dir, r, c, mxval;
+		public Cell(int id, int r, int c, int val, int dir) {
+			this.id = id;
+			this.val = val;
+			this.dir = dir;
+			this.r = r;
+			this.c = c;
+			this.mxval = val;
+		}
+		
+		void move(int[][] board){
+			// 해당 방향으로 이동
+			// 이동한자리가 끝자리면 val / 2
+			// 이동한 자리에 다른 놈 있으면 합치기
+			int nr = r + dx[dir];
+			int nc = c + dy[dir];
+			if(0 <= nr && nr < N && 0 <= nc && nc < N) {
+				if(nr == 0 || nr == N - 1 || nc == 0 || nc == N - 1) {
+					val /= 2;
+					if(val == 0) {
+						cells[this.id] = null;
+						return;
 					}
 				}
-				for(Cell c : cells) {
-					if(c == null) continue;
-					c.mxSize = c.size;
-				}
+				r = nr;
+				c = nc;
+			}else {
+				if(dir == 1) dir = 2;
+				else if(dir == 2) dir = 1;
+				else if(dir == 3) dir = 4;
+				else if(dir == 4) dir = 3;
+				r += dx[dir];
+				c += dy[dir];
 			}
-			int total = 0 ;
-			for(Cell c : cells) {
-				if(c == null) continue;
-				total += c.size;
+			
+			if(board[r][c] != 0) {
+				if (cells[board[r][c]].mxval > this.val) {
+					cells[board[r][c]].val += this.val;
+					cells[this.id] = null;
+				}else {
+					this.val += cells[board[r][c]].val;
+					cells[board[r][c]] = null;
+					board[r][c] = this.id;
+				}
+			}else {
+				board[r][c] = this.id;
+			}
+		}
+		
+	}
+	static int[] dx = {0, -1, 1, 0, 0};
+	static int[] dy = {0, 0, 0, -1, 1};
+	static int N, M, K;
+	static Cell[] cells;
+	public static void main(String[] args) throws IOException {
+		int TC = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= TC; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			map = new int[N][N];
+			cells = new Cell[K + 1];
+			for(int i = 1 ; i < K + 1 ; i++) {
+				st = new StringTokenizer(br.readLine());
+				int r = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				int val = Integer.parseInt(st.nextToken());
+				int dir = Integer.parseInt(st.nextToken());
+				cells[i] = new Cell(i, r, c, val, dir);
+				map[r][c] = i;
+			}
+			for(int t = 0 ; t < M ; t++) {
+				int[][] tmpmap = new int[N][N];
+				for(Cell cell : cells) {
+					if(cell == null) continue;
+					cell.move(tmpmap);
+				}
+				for(Cell cell : cells) {
+					if(cell == null) continue;
+					cell.mxval = cell.val;
+				}
+				map = tmpmap;
+			}
+			int total = 0;
+			for(Cell cell : cells) {
+				if(cell == null) continue;
+				total += cell.val;
 			}
 			bw.write("#" + tc + " " + total + "\n");
+			
 		}
-		bw.flush();
+		
+		bw.close();
 	}
+
 }
