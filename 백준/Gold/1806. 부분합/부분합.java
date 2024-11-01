8116cacb4781a413d193b4d106829016bd47dc77
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-11-01 23:37:03 +0900

    [Gold IV] Title: 부분합, Time: 208 ms, Memory: 22892 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException{
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		int[] nums = new int[N + 1];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1 ; i < N + 1 ; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		int[] dp = new int[N + 1];
+		dp[1] = nums[1];
+		for(int i = 2 ; i < N + 1 ; i++) {
+			dp[i] = dp[i - 1] + nums[i];
+		}
+		int start = 0;
+		int end = 1;
+		int ans = Integer.MAX_VALUE;
+		while(start <= end) {
+			if(end == N + 1) {
+				if(ans == Integer.MAX_VALUE) {
+					ans = 0;
+				}
+				break;
+			}
+			int sum = dp[end] - dp[start];
+//			System.out.println(sum);
+			if(sum >= S) {
+				ans = Math.min(ans, end - start);
+			}
+			if(sum < S) {
+				end++;
+			}else {
+				start++;
+			}
+		}
+//		System.out.println(start +  " " + end);
+		bw.write(ans + "");
+		bw.close();
+	}
+}
