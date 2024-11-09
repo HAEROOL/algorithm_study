16f3896396c4bafc9f4bdf34fa624d33d85a3239
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-11-09 22:36:48 +0900

    [Gold II] Title: 색종이 붙이기, Time: 168 ms, Memory: 18776 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -1,80 +1,69 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int ans = Integer.MAX_VALUE;
-	static int[] paper = {0, 5, 5, 5, 5, 5};
-	static int[][] map;
-	static void backtracking(int x, int y, int cnt) {
-//		System.out.println(cnt);
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	static int[][] map = new int[10][10];
+	static boolean checkMarking(int x, int y, int r) {
+		if(x + r > 10 || y + r > 10) return false;
+		for(int i = x ; i < x + r ; i++) {
+			for(int j = y ; j < y + r ; j++) {
+				if(map[i][j] == 0) return false;
+			}
+		}
+		return true;
+	}
+	static void marking(int x, int y, int r, int f) {
+		for(int i = x ; i < x + r ; i++) {
+			for(int j = y ; j < y + r ; j++) {
+				map[i][j] = f;
+			}
+		}
+	}
+	static void backtracking(int x, int y, int[] paper, int cnt) {
 		if(x >= 9 && y > 9) {
-//			System.out.println(cnt);
-			ans = Math.min(ans, cnt);
+			ans = Math.min(cnt, ans);
 			return;
 		}
-		if(ans <= cnt) {
-			return;
-		}
+//		for(int[] row:map) {
+//			System.out.println(Arrays.toString(row));
+//		}
+//		System.out.println(x + " " + y);
+//		System.out.println(Arrays.toString(paper));
+		if(ans <= cnt) return;
+		
 		if(y > 9) {
-			backtracking(x + 1, 0, cnt);
+			backtracking(x + 1, 0, paper, cnt);
 			return;
 		}
 		if(map[x][y] == 1) {
 			for(int i = 5 ; i > 0 ; i--) {
-//				System.out.println(check(x, y, i));
-				if(paper[i] > 0 && check(x, y, i)) {
+//				System.out.println(checkMarking(x, y, i));
+				if(paper[i] > 0 && checkMarking(x, y, i)) {
 					marking(x, y, i, 0);
 					paper[i]--;
-					backtracking(x, y + 1, cnt + 1);
+					backtracking(x, y + 1, paper, cnt + 1);
 					marking(x, y, i, 1);
-					paper[i]++;					
+					paper[i]++;
 				}
 			}
 		}else {
-			backtracking(x, y + 1, cnt);
+			backtracking(x, y + 1, paper, cnt);
 		}
 	}
-	static boolean check(int x, int y, int size) {
-	    for(int i = x ; i <  x + size ; i++){
-	        for(int j = y ; j < y + size ; j++){
-	          if(i < 0 || i >= 10 || j < 0 || j>= 10) return false;
-	          if(map[i][j] != 1) return false;
-	        }
-	      }
-	      return true;
-	}
-	static void marking(int x, int y, int k, int state) {
-		for(int i = x ; i < x + k ; i++) {
-			for(int j = y ; j < y + k ; j++) {
-				map[i][j] = state;
-			}
-		}
-	}
+	static int ans = Integer.MAX_VALUE;
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
 		StringTokenizer st;
-		map = new int[10][10];
-
-		for (int i = 0; i < 10; i++) {
+		for(int i = 0 ; i < 10 ; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < 10; j++) {
+			for(int j = 0 ; j < 10 ; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		backtracking(0, 0, 0);
-		ans = ans == Integer.MAX_VALUE?-1:ans;
-		bw.write(ans + "");
-		bw.close();
+		backtracking(0, 0, new int[] {0, 5, 5, 5, 5, 5}, 0);
+		System.out.println(ans==Integer.MAX_VALUE?-1:ans);
 	}
+
 }
