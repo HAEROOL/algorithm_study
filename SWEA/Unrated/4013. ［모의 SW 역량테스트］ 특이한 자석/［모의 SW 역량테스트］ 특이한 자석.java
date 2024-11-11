ec92c582c5769dffaee83c3ce7e21dee587ee848
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-11-11 22:26:25 +0900

    [Unrated] Title: [모의 SW 역량테스트] 특이한 자석, Time: 121 ms, Memory: 18,872 KB -BaekjoonHub

--- SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
+++ SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
@@ -5,60 +5,49 @@ public class Solution {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static List<Integer>[] gears;
-	static boolean[] v;
-	static void rotate(int gear, int dir) {
-		v[gear] = true;
-		// 오른쪽으로
+	static void rotate(int n, int dir) {
+		v[n] = true;
+		if(n < 3 && gears[n].get(2) != gears[n + 1].get(6) && !v[n + 1]) {
+			rotate(n + 1, dir * -1);
+		}
+		if(n > 0 && gears[n].get(6) != gears[n - 1].get(2) && !v[n - 1]) {
+			rotate(n - 1, dir * -1);
+		}
 		if(dir == 1) {
-			int ngear = gear + 1;
-			if(ngear < 4 && !v[ngear] && gears[gear].get(2) != gears[ngear].get(6)) {
-				rotate(ngear, dir * -1);
-			}
-			ngear = gear - 1;
-			if(ngear >= 0 && !v[ngear] && gears[gear].get(6) != gears[ngear].get(2)) {
-				rotate(ngear, dir * -1);
-			}
-			gears[gear].add(0, gears[gear].remove(gears[gear].size() - 1));
+			gears[n].add(0, gears[n].remove(7));
 		}else {
-			int ngear = gear + 1;
-			if(ngear < 4 && !v[ngear] && gears[gear].get(2) != gears[ngear].get(6)) {
-				rotate(ngear, dir * -1);
-			}
-			ngear = gear - 1;
-			if(ngear >= 0 && !v[ngear]&& gears[gear].get(6) != gears[ngear].get(2)) {
-				rotate(ngear, dir * -1);
-			}
-			gears[gear].add(gears[gear].size() - 1, gears[gear].remove(0));
+			gears[n].add(7, gears[n].remove(0));
 		}
 	}
+	static boolean[] v;
 	public static void main(String[] args) throws IOException {
-		  int TC = Integer.parseInt(br.readLine());
-		  for(int tc = 1 ; tc < TC + 1 ; tc++) {
-			  int ans = 0;
-			  gears = new ArrayList[4];
-			  int K = Integer.parseInt(br.readLine());
-			  StringTokenizer st;
-			  for(int i = 0 ; i < 4 ; i++) {
-				  st = new StringTokenizer(br.readLine());
-				  gears[i] = new ArrayList<>();
-				  for(int j = 0 ; j < 8 ; j++) {
-					  gears[i].add(Integer.parseInt(st.nextToken()));
-				  }
-			  }
-			  for(int i = 0 ; i < K ; i++) {
-				  st = new StringTokenizer(br.readLine());
-				  int g = Integer.parseInt(st.nextToken()) - 1;
-				  int dir = Integer.parseInt(st.nextToken());
-				  v = new boolean[4];
-				  rotate(g, dir);
-			  }
-			  for(int i = 0 ; i < 4 ; i++) {
-				  if(gears[i].get(0) == 1) {
-					  ans += Math.pow(2, i);
-				  }
-			  }
-			  bw.write("#" + tc +" " + ans + "\n");
-		  }
-		  bw.close();
+		int TC = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc < TC + 1; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int K = Integer.parseInt(st.nextToken());
+			gears = new ArrayList[4];
+			for(int i = 0 ; i < 4 ; i++) {
+				st = new StringTokenizer(br.readLine());
+				gears[i] = new ArrayList<>();
+				for(int j = 0 ; j < 8 ; j++) {
+					gears[i].add(Integer.parseInt(st.nextToken()));
+				}
+			}
+			for(int i = 0 ; i < K ; i++) {
+				st = new StringTokenizer(br.readLine());
+				v = new boolean[4];
+				int target = Integer.parseInt(st.nextToken());
+				int dir = Integer.parseInt(st.nextToken());
+				rotate(target - 1, dir);
+			}
+			int ans = 0;
+			for(int i = 0 ; i < 4 ; i++) {
+				if(gears[i].get(0) == 1) {
+					ans += Math.pow(2, i);
+				}
+			}
+			bw.write("#" + tc + " " + ans + "\n");
+		}
+		bw.close();
 	}
 }
