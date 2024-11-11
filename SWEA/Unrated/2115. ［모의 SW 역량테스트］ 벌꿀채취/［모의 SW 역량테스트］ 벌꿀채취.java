6b9cb6a6c59f03224aacc4e4d0db05d960deb56a
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-11-11 23:36:55 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 116 ms, Memory: 18,952 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -4,57 +4,73 @@ import java.util.*;
 public class Solution {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static void subset(List<Integer> list, int idx, int total, int value) {
-		if(total > C) {
-			return;
-		}
-		if( idx >= list.size()) {
-			vmx = Math.max(vmx, value);
-			return;
-		}
-		subset(list, idx + 1, total, value);
-		value = value + list.get(idx) * list.get(idx);
-		total = total + list.get(idx);
-		subset(list, idx + 1, total ,value);
-	}
 	static int N, M, C;
-	static int vmx;
+	static int[][] map;
+	static void subset(boolean[] sel, int idx, int total, int[] arr, int flag) {
+		if(total > C) return;
+		if(idx == sel.length) {
+			int t = 0;
+			for(int i = 0 ; i < sel.length ; i++) {
+				if(sel[i]) {
+					t += arr[i] * arr[i];
+				}
+			}
+			if(flag == 0) {
+				asub = Math.max(asub, t);				
+			}else {
+				bsub = Math.max(bsub, t);
+			}
+			return;
+		}
+		
+		sel[idx] = true;
+		subset(sel, idx + 1, total + arr[idx], arr, flag);
+		sel[idx] = false;
+		subset(sel, idx + 1, total, arr, flag);
+	}
+	static int asub = 0;
+	static int bsub = 0;
+	static int ans = 0;
 	public static void main(String[] args) throws IOException {
 		int TC = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= TC; tc++) {
+		for (int tc = 1; tc < TC + 1; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			C = Integer.parseInt(st.nextToken());
-			int[][] map = new int[N][N];
-			for (int i = 0; i < N; i++) {
+			map = new int[N][N];
+			ans = 0;
+			for(int i = 0; i < N ; i++) {
 				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
+				for(int j = 0 ; j < N ; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			List<Integer> ans = new ArrayList<>();
-			for (int k = 0; k < N; k++) {
-				vmx = -1;
-				for (int i = 0; i < N; i++) {
-					List<Integer> list = new ArrayList<>();
-					for (int j = i; j < i + M; j++) {
-						if (j>=N) break;
-						list.add(map[k][j]);
+			
+			for(int i = 0 ; i < N ; i++) {
+				asub = 0;
+				//A subset 최댓값 구하
+				for(int j = 0 ; j < N - M + 1 ; j++) {
+					int[] arr = new int[M];
+					for(int k = 0 ; k < M ; k++) {
+						arr[k] = map[i][j + k];
 					}
-//					System.out.println(list);
-					subset(list, 0, 0, 0);
+					subset(new boolean[M], 0, 0, arr, 0);
 				}
-//				System.out.println(vmx);
-				ans.add(vmx);
+				for(int b = i + 1 ; b < N ; b++) {
+					for(int j = 0 ; j < N - M + 1 ; j++) {
+						bsub = 0;
+						int[] arr = new int[M];
+						for(int k = 0 ; k < M ; k++) {
+							arr[k] = map[b][j + k];
+						}
+						subset(new boolean[M], 0, 0, arr, 1);
+						ans = Math.max(ans, asub + bsub);
+					}					
+				}
 			}
-//			System.out.println(ans);
-			Collections.sort(ans);
-//			System.out.println(ans);
-
-			bw.write("#" + tc + " " + (ans.get(ans.size() - 1) + ans.get(ans.size() - 2)) + "\n");
+			bw.write("#" + tc + " " + ans + "\n");
 		}
 		bw.close();
 	}
-
 }
