a19150ecebfde49dc8585cc6a75f7aa1a18e0c2e
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-11-11 21:53:32 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 240 ms, Memory: 41,988 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -4,62 +4,63 @@ import java.util.*;
 public class Solution {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static void combination(boolean[] sel, int k, int depth) {
-		if(k == N/2) {
-			cal(sel);
+	static void comb(boolean[] sel, int idx, int cnt) {
+		if(idx == N) {
+			if(cnt == N / 2) {
+				List<Integer> a = new ArrayList<>();
+				List<Integer> b = new ArrayList<>();
+				for(int i = 0 ; i < N ; i++) {
+					if(sel[i]) a.add(i);
+					else b.add(i);
+				}
+				int atotal = 0;
+				for(int i = 0 ; i < a.size() ; i++) {
+					for(int j = i + 1 ; j < a.size() ; j++) {
+						int x = a.get(i);
+						int y = a.get(j);
+						atotal += map[x][y];
+						atotal += map[y][x];
+					}
+				}
+				int btotal = 0;
+				for(int i = 0 ; i < b.size() ; i++) {
+					for(int j = i + 1 ; j < b.size() ; j++) {
+						int x = b.get(i);
+						int y = b.get(j);
+						btotal += map[x][y];
+						btotal += map[y][x];
+					}
+				}
+				ans = Math.min(ans, Math.abs(atotal - btotal));
+			}
 			return;
 		}
-		if(depth > N) return;
-		sel[depth] = true;
-		combination(sel, k + 1, depth + 1);
-		sel[depth] = false;
-		combination(sel, k, depth + 1);
 		
+		sel[idx] = true;
+		comb(sel, idx + 1, cnt + 1);
+		sel[idx] = false;
+		comb(sel, idx + 1, cnt);
 	}
-	static void cal(boolean[] sel) {
-		List<Integer> lista = new ArrayList<>();
-		List<Integer> listb = new ArrayList<>();
-		for(int i = 1 ; i < N + 1 ; i++) {
-			if(sel[i]) lista.add(i);
-			else listb.add(i);
-		}
-		int totala = 0;
-		int totalb = 0;
-//		System.out.println(lista);
-//		System.out.println(listb);
-//		System.out.println();
-		for(int i = 0 ; i < lista.size() ; i++) {
-			for(int j = i + 1 ; j < lista.size() ; j++) {
-				int ai = lista.get(i), aj = lista.get(j);
-				int bi = listb.get(i), bj = listb.get(j);
-				int suma = map[ai][aj] + map[aj][ai];
-				int sumb = map[bi][bj] + map[bj][bi];
-				totala += suma;
-				totalb += sumb;
-			}
-		}
-//		System.out.println(totala + " " + totalb);
-		ans = Math.min(ans, Math.abs(totala - totalb));
-	}
-	static int ans;
 	static int N;
 	static int[][] map;
+	static int ans;
 	public static void main(String[] args) throws IOException {
-		  int TC = Integer.parseInt(br.readLine());
-		  for(int tc = 1 ; tc < TC + 1 ; tc++) {
-			  StringTokenizer st = new StringTokenizer(br.readLine());
-			  N = Integer.parseInt(st.nextToken());
-			  ans = Integer.MAX_VALUE;
-			  map = new int[N + 1][N + 1];
-			  for(int i = 1 ; i < N + 1 ; i++) {
-				  st = new StringTokenizer(br.readLine());
-				  for(int j = 1 ; j < N + 1 ; j++) {
-					  map[i][j] = Integer.parseInt(st.nextToken());
-				  }
-			  }
-			  combination(new boolean[N + 1], 0, 1);
-			  bw.write("#" + tc +" " + ans + "\n");
-		  }
-		  bw.close();
+		int TC = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc < TC + 1; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			map = new int[N][N];
+			ans = Integer.MAX_VALUE;
+			for(int i = 0 ; i < N ; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0 ; j < N ; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+//			for(int[] row : map)System.out.println(Arrays.toString(row));
+			comb(new boolean[N], 0, 0);
+			bw.write("#" + tc + " " + ans + "\n");
+		}
+		bw.close();
 	}
 }
