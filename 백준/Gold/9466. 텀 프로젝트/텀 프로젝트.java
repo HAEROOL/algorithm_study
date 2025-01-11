6c4987eaadd19e6465f201c21f74f7cf1e412ce9
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2025-01-11 19:09:40 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1068 ms, Memory: 303956 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -4,41 +4,39 @@ import java.util.*;
 public class Main {
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static boolean[] v, isDone;
+	static int[] degrees;
 	static int[] stus;
-	static int ans;
-	static void dfs(int n) {
-		v[n] = true;
-		int next = stus[n];
-		if(!v[next]) dfs(next);
-		else {
-			if(!isDone[next]) {
-				ans--;
-				while(next != n) {
-					next = stus[next];
-					ans--;
-				}
-			}
-		}
-		isDone[n] = true;
-	}
 	public static void main(String[] args) throws IOException {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int TC = Integer.parseInt(st.nextToken());
 		for(int tc = 0 ; tc < TC ; tc++) {
 			int N = Integer.parseInt(br.readLine());
+			degrees = new int[N + 1];
 			stus = new int[N + 1];
-			ans = N;
-			isDone = new boolean[N + 1];
-			v = new boolean[N + 1];
 			st = new StringTokenizer(br.readLine());
 			for(int i = 1 ; i < N + 1 ; i++) {
-				stus[i] = Integer.parseInt(st.nextToken());
+				int n = Integer.parseInt(st.nextToken());
+				stus[i] = n;
+				degrees[n]++;
 			}
+			Deque<Integer> q = new ArrayDeque();
 			for(int i = 1 ; i < N + 1 ; i++) {
 				// 방문했는지
-				if(v[i]) continue;
-				dfs(i);
+				if(degrees[i] == 0) {
+					q.add(i);
+				}
+			}
+			while(!q.isEmpty()) {
+				int n = q.poll();
+				int next = stus[n];
+				degrees[next]--;
+				if(degrees[next] == 0) {
+					q.add(next);
+				}
+			}
+			int ans = N;
+			for(int s : degrees) {
+				if(s != 0) ans--;
 			}
 			bw.write(ans + "\n");
 		}
