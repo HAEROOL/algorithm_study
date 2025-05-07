5e7a55e295af0aa783f8e4afad6d103809900d83
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2025-05-07 22:19:03 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 180.21 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.js
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.js
@@ -2,32 +2,31 @@ function solution(s)
 {
     var answer = 1;
     
-    const dp = [];
-    for(let i = 0 ; i < s.length ; i++){
-        dp[i] = []
-        for(let j = 0 ; j < s.length; j++){
-            dp[i].push(false);
-            if(i === j) dp[i][j] = true;
-        }
-    }
-    for(let i = 0 ; i < s.length - 1; i++){
+   const dp = []
+   for(let i = 0 ; i < s.length ; i++){
+       dp[i] = [];
+       for(let j = 0 ; j < s.length ; j++){
+           dp[i].push(false);
+           if(i === j)dp[i][j] = true;
+       }
+   }
+    
+    for(let i = 0 ; i < s.length - 1;i++){
         if(s[i] === s[i + 1]){
             dp[i][i + 1] = true;
-            answer = 2
+            answer = 2;
         }
     }
     
-    for(let i = 2 ; i < s.length + 1 ; i++){
-        for(let start = 0 ; start <= s.length - i ; start++){
-            const end = i + start - 1;
-            if(s[start] === s[end] && dp[start + 1][end - 1]){
-                dp[start][end] = true;
+    for(let i = 3 ; i <= s.length ; i++){
+        for(let j = 0 ; j < s.length - i + 1 ; j++){
+            const end = j + i - 1;
+            if(s[j] === s[end] && dp[j + 1][end - 1]){
+                dp[j][end] = true;
                 answer = i;
             }
         }
     }
-    
-    // console.log(dp)
     
     return answer;
 }
