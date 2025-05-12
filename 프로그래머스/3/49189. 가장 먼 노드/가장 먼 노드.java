823714bd71df05a9313ac46250646ef72a595f40
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2025-05-12 21:53:11 +0900

    [level 3] Title: 가장 먼 노드, Time: 70.28 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -4,41 +4,7 @@ class Solution {
     static List<Integer>[] map;
     static int N;
     static int answer = 0;
-    static void dijkstra(int start){
-        int[] dist = new int[N + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        boolean[] v = new boolean[N + 1];
-        
-        dist[start] = 0;
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> {
-            return a[1] - b[1];
-        });
-        pq.offer(new int[]{start, 0});
-        while(!pq.isEmpty()){
-            int[] n = pq.poll();
-            int now = n[0];
-            
-            if(v[now]) continue;
-            v[now] = true;
-            for(int next : map[now]){
-                int nextNode = next;
-                int nextCost = 1;
-                if(dist[nextNode] > dist[now] + nextCost){
-                    pq.offer(new int[]{nextNode, dist[now] + 1});
-                    dist[nextNode] = dist[now] + 1;
-                }
-            }
-        }
-        int mx = -1;
-        for(int i : dist){
-            if(i == Integer.MAX_VALUE) continue;
-            mx = Math.max(i, mx);
-        }
-        for(int i : dist){
-            if(i == mx) answer++;
-        }
-         
-    }
+    
     public int solution(int n, int[][] edge) {
         answer = 0;
         N = n;
@@ -54,7 +20,28 @@ class Solution {
             map[b].add(a);
         }
         
-        dijkstra(1);
+        int[] v = new int[n + 1];
+        v[1] = 1;
+        Deque<Integer> q = new ArrayDeque<Integer>();
+        q.offer(1);
+        
+        int mx = -1;
+        while(!q.isEmpty()){
+            int now = q.poll();
+            mx = Math.max(mx, v[now]);
+            for(int next : map[now]){
+                if(v[next] == 0){
+                    v[next] = v[now] + 1;
+                    q.offer(next);
+                }
+            }
+        }
+        // System.out.println(Arrays.toString(v) + " " + mx);
+        for(int i : v){
+            if(mx == i) answer++;
+        }
+        
+        
         return answer;
     }
 }
