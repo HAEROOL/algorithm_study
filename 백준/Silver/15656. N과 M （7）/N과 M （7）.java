059cfb0d68be28014b2491380c50016cd7232563
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2025-05-13 15:09:25 +0900

    [Silver III] Title: N과 M (7), Time: 820 ms, Memory: 300924 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.java
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -1,42 +1,41 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-  static int[] arr;
-  static public void recursive(int k, int idx, int[] sel) throws IOException {
-    if(idx == M){
-      for(int i : sel){
-        bw.write(arr[i] + " ");
-      }
-      bw.write("\n");
-      return;
-    }
-    if(k > N){
-      return;
-    }
-    for(int i = 0 ; i < N ; i++){
-          sel[idx] = i;
-          recursive(i, idx + 1, sel);
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-      }
-    }
+	static void permutation(int[] sel, int idx) throws IOException {
+		if (idx == sel.length) {
+			for (int i : sel) {
+				bw.write(arr[i] + " ");
+			}
+			bw.write("\n");
+			return;
+		}
+		for (int i = 0; i < N; i++) {
+			sel[idx] = i;
+			permutation(sel, idx + 1);
+		}
 
-  static int N, M;
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-    arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-    N = input[0];
-    M = input[1];
-    Arrays.sort(arr);
-    // N 개중 M개 고르기
-    recursive(0, 0, new int[M]);
-    bw.flush();
-    bw.close();
-  }
+	}
+
+	static int[] arr;
+	static int ans;
+	static int N, M;
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		permutation(new int[M], 0);
+
+		bw.close();
+	}
 }
