ac96a5a2d07af8c4000bc4befed6dcd280818a6d
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2025-09-10 21:03:45 +0900

    [level 1] Title: 실패율, Time: 4.96 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
 
 ### 성능 요약
 
-메모리: 102 MB, 시간: 468.34 ms
+메모리: 86.6 MB, 시간: 4.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 03일 20:02:56
+2025년 09월 10일 21:03:42
 
 ### 문제 설명
 
--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -2,36 +2,39 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        double[][] tmp = new double[N][2];
-        for(int i = 0 ; i < N ; i++){
-            int stage = i + 1;
-            int notClearCount = 0;
-            int totalCount = 0;
-            for(int j = 0 ; j < stages.length ; j++){
-                if(stages[j] == stage){
-                    notClearCount++;
-                    totalCount++;
-                }else if(stages[j] > stage){
-                    totalCount++;
-                }
-            }
-            double rate = totalCount != 0 ? (double)notClearCount / (double)totalCount : 0;
-            // System.out.println(stage + " " + totalCount + " " + notClearCount + " " + rate);
-            tmp[i] = new double[]{stage, rate};
-        }
-        Arrays.sort(tmp, (a, b) -> {
-            if(a[1] == b[1]){
-                return Double.compare(a[0], b[0]);
-            }
-            return Double.compare(b[1], a[1]);
-        });
-        // for(double[] row : tmp){
-        //     System.out.println(Arrays.toString(row));
-        // }
         int[] answer = new int[N];
-        for(int i = 0 ; i < N ; i++){
-            answer[i] = (int)tmp[i][0];
+        int[] counts = new int[N + 2]; // 1~N+1 까지 저장용 (편의상)
+        
+        // 각 스테이지별 도전자 수 카운트
+        for (int s : stages) {
+            counts[s]++;
         }
+
+        double[][] res = new double[N][2];
+        int total = stages.length; // 현재까지 도전한 사람 수
+
+        for (int i = 1; i <= N; i++) {
+            double failRate = 0.0;
+            if (total > 0) {
+                failRate = (double) counts[i] / total;
+            }
+            res[i - 1][0] = i; // 스테이지 번호
+            res[i - 1][1] = failRate; // 실패율
+            total -= counts[i]; // i스테이지에 머무른 사람 제외
+        }
+
+        // 실패율 기준 정렬
+        Arrays.sort(res, (a, b) -> {
+            if (a[1] == b[1]) {
+                return Double.compare(a[0], b[0]); // 스테이지 번호 오름차순
+            }
+            return Double.compare(b[1], a[1]); // 실패율 내림차순
+        });
+
+        for (int i = 0; i < N; i++) {
+            answer[i] = (int) res[i][0];
+        }
+        
         return answer;
     }
 }
