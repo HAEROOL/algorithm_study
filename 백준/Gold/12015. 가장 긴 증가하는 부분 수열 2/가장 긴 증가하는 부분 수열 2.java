d6fb0de732aaf50dbfcf82430e7fc6b42024c6e8
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2025-09-15 22:38:28 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 444 ms, Memory: 125436 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int lis = 0;
+	static int[] dp;
+	static int binSearch(int target, int start, int end, int size) {
+		int res = -1;
+		while(start <= end) {
+			int mid = (start + end) / 2;
+			if(dp[mid] >= target) {
+				end = mid - 1;
+				res = mid;
+			}else {
+				start = mid + 1;
+			}
+		}
+		if(start == size) {
+			return -1;
+		}else {
+			return res;
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] nums = new int[N];
+		for(int i = 0 ; i < N ; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		dp = new int[N];
+		
+		for(int i = 0 ; i < N ; i++) {
+			int pos = binSearch(nums[i], 0, lis, lis + 1);
+			if(pos == -1) {
+				dp[lis] = nums[i];
+				lis++;
+			}else {
+				dp[pos] = nums[i];
+			}
+		}
+		bw.write(lis + "");
+		bw.close();
+	}
+}
