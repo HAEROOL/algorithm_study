a31aa5d49ab3be1a15b059af73659e53ddcdeb14
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2025-09-22 20:14:37 +0900

    [Gold II] Title: 보석 도둑, Time: 1700 ms, Memory: 155100 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,51 +1,48 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
+		long answer = 0;
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		int[][] vals = new int[N][2];
+		int[][] jewels = new int[N][2];
+		int[] bags = new int[K];
+		
 		for(int i = 0 ; i < N ; i++) {
 			st = new StringTokenizer(br.readLine());
 			int m = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
-			vals[i] = new int[] {m, v};
+			jewels[i] = new int[] {m, v};
 		}
-		int[] knapsack = new int[K];
 		for(int i = 0 ; i < K ; i++) {
-			knapsack[i] = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			bags[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		Arrays.sort(vals, (a, b) -> {
-			if(a[0] == b[0]) {
+		Arrays.sort(jewels, (a, b) -> {
+			if(b[0] == a[0]) {
 				return b[1] - a[1];
 			}
 			return a[0] - b[0];
 		});
-		Arrays.sort(knapsack);
+		Arrays.sort(bags);
 		
-		PriorityQueue<Integer> q = new PriorityQueue<>(Comparator.reverseOrder());
-		long ans = 0;
-
-		
-		int idx = 0;
-		for(int i = 0; i < K ; i++) {
-			while(idx < N && vals[idx][0] <= knapsack[i]) {
-				q.offer(vals[idx][1]);
-				idx++;
+		PriorityQueue<Integer> q = new PriorityQueue<>((a, b) -> b - a);
+		int j = 0;
+		for(int i = 0 ; i < K ; i++) {
+			int bag = bags[i];
+			while(j < N && bag >= jewels[j][0]) {
+				q.offer(jewels[j++][1]);
 			}
-			
 			if(!q.isEmpty()) {
-				ans += q.poll();
+				answer += q.poll();
 			}
 		}
-		
-		
-		bw.write(ans + "");
+		bw.write(answer + "");
 		bw.close();
 	}
 }
