78bdf1e361b446ecd346bf20db03b6d0d891a8bd
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2025-09-29 22:42:35 +0900

    [level 3] Title: 불량 사용자, Time: 98.44 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/README.md
+++ 프로그래머스/3/64064. 불량 사용자/README.md
@@ -1,10 +1,10 @@
 # [level 3] 불량 사용자 - 64064 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064) 
 
 ### 성능 요약
 
-메모리: 118 MB, 시간: 102.52 ms
+메모리: 118 MB, 시간: 98.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 05일 20:37:52
+2025년 09월 29일 22:42:32
 
 ### 문제 설명
 
--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,48 +1,41 @@
 import java.util.*;
 
 class Solution {
-    static String[] userId, bannedId;
-    static boolean[] used;       // userId 중복 사용 방지
-    static Set<Set<String>> results = new HashSet<>();
-
-    // 불량패턴 매칭 함수: '*'는 정확히 한 글자 와일드카드
-    static boolean isBad(String pat, String id) {
-        if (pat.length() != id.length()) return false;
-        for (int i = 0; i < pat.length(); i++) {
-            char pc = pat.charAt(i), uc = id.charAt(i);
-            if (pc != '*' && pc != uc) return false;
-        }
-        return true;
-    }
-
-    // bannedId 순서대로 하나씩 대응할 유저를 골라보는 DFS
-    static void dfs(int idx, Set<String> picked) {
-        if (idx == bannedId.length) {
-            // bannedId 전부 매칭 완료 → 결과 집합에 추가
-            results.add(new HashSet<>(picked));
+    static String[] bans, users;
+    static Set<Set<String>> resultSet = new HashSet<>();
+    
+    static void combination(int banIdx, Set<String> selected) {
+        if (banIdx == bans.length) {
+            resultSet.add(new HashSet<>(selected));
             return;
         }
-        String pat = bannedId[idx];
-        for (int i = 0; i < userId.length; i++) {
-            if (used[i]) continue;
-            if (isBad(pat, userId[i])) {
-                used[i] = true;
-                picked.add(userId[i]);
-
-                dfs(idx + 1, picked);
-
-                used[i] = false;
-                picked.remove(userId[i]);
+        for (String user : users) {
+            if (selected.contains(user)) continue;
+            if (isSame(user, bans[banIdx])) {
+                selected.add(user);
+                combination(banIdx + 1, selected);
+                selected.remove(user);
             }
         }
     }
-
+    
+    static boolean isSame(String user, String ban) {
+        if (user.length() != ban.length()) return false;
+        
+        for (int i = 0; i < user.length(); i++) {
+            if (ban.charAt(i) == '*') continue;
+            if (user.charAt(i) != ban.charAt(i)) return false;
+        }
+        return true;
+    }
+    
     public int solution(String[] user_id, String[] banned_id) {
-        userId = user_id;
-        bannedId = banned_id;
-        used = new boolean[userId.length];
-
-        dfs(0, new HashSet<>());
-        return results.size();
+        bans = banned_id;
+        users = user_id;
+        resultSet = new HashSet<>();
+        
+        combination(0, new HashSet<>());
+        
+        return resultSet.size();
     }
 }
