120e8c0a1bdd4377894c53a58b49a3b7886d3900
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2025-10-01 23:51:25 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 160 ms, Memory: 110792 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -4,26 +4,21 @@ inf = float('inf')
 
 fmap = [[inf for _ in range(N + 1)]for _ in range(N + 1)]
 
-
 for _ in range(M):
     a, b = map(int, input().split())
     fmap[a][b] = 1
     fmap[b][a] = 1
 
+for x in range(1, N + 1):
+    fmap[x][x] = 0
+
 for k in range(1, N + 1):
     for i in range(1, N + 1):
-        for j in range(i + 1, N + 1):
+        for j in range(1, N + 1):
             fmap[i][j] = min(fmap[i][j], fmap[i][k] + fmap[k][j])
-            fmap[j][i] = fmap[i][j]
 
-answer = [0, inf]
-
+arr = []
 for x in range(1, N + 1):
-    total = 0
-    for y in fmap[x]:
-        if y != inf:
-            total += y
-    
-    if answer[1] > total:
-        answer = [x, total]
-print(answer[0])
+    arr.append((sum(fmap[x][1:]), x))
+arr.sort()
+print(arr[0][1])
