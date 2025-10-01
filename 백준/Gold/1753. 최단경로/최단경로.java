e52620841e7e3da63f3b6a547e709e730ead566d
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2025-10-01 23:39:41 +0900

    [Gold IV] Title: 최단경로, Time: 728 ms, Memory: 122620 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,62 +1,61 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static int V, E;
 	static List<int[]>[] map;
+	static int V, E;
 	static int[] dijkstra(int start) {
-		int[] dist = new int[V + 1];
 		boolean[] v = new boolean[V + 1];
+		int[] dist = new int[V + 1];
 		Arrays.fill(dist, Integer.MAX_VALUE);
 		
-		PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-		q.add(new int[] {start, 0});
+		PriorityQueue<int[]> q = new PriorityQueue<int[]>((a, b) -> {
+			return a[1] - b[1];
+		});
 		dist[start] = 0;
+		q.offer(new int[] {start, 0});
 		
 		while(!q.isEmpty()) {
 			int[] n = q.poll();
-			int now = n[0];
-			
-			if(v[now]) continue;
-			v[now] = true;
-			for(int[] next : map[now]) {
-				int nextnode = next[0];
-				int nextcost = next[1];
-				if(dist[nextnode] > dist[now] + nextcost) {
-					dist[nextnode] = dist[now] + nextcost;
-					q.add(new int[] {nextnode, dist[nextnode]});
+			int node = n[0];
+			if(v[node]) continue;
+			v[node] = true;
+			for(int[] next : map[node]) {
+				int nextNode = next[0];
+				int nextCost = next[1];
+				if(dist[nextNode] > dist[node] + nextCost) {
+					dist[nextNode] = dist[node] + nextCost;
+					q.offer(new int[] {nextNode, dist[nextNode]});
 				}
 			}
 		}
 		return dist;
 	}
 	public static void main(String[] args) throws IOException {
-		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
-		int start = Integer.parseInt(br.readLine());
 		
-		map = new ArrayList[V + 1];
-		for(int i = 1 ; i < V + 1 ; i++) {
-			map[i] = new ArrayList<>();
+		int start = Integer.parseInt(br.readLine());
+		map = new List[V + 1];
+		for(int i = 0 ; i < V + 1 ; i++) {
+			map[i] = new ArrayList<int[]>();
 		}
 		
 		for(int i = 0 ; i < E ; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
-			int to =Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
 			int cost = Integer.parseInt(st.nextToken());
-			map[from].add(new int[] {to, cost});
+			
+			map[from].add(new int[]{to, cost});
 		}
-		
-		int[] res = dijkstra(start);
+		int[] answer = dijkstra(start);
 		for(int i = 1 ; i < V + 1 ; i++) {
-			int e = res[i];
-			bw.write((e==Integer.MAX_VALUE?"INF":e) +"\n");
+			bw.write((answer[i] == Integer.MAX_VALUE?"INF":answer[i]) + "\n");
 		}
 		bw.close();
 	}
-
 }
