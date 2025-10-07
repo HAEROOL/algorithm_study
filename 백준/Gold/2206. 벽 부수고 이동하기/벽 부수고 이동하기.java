f3259b408f75cd023ecbaf0146a0a4e81b31896f
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2025-10-07 16:53:58 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1136 ms, Memory: 188524 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,57 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-  static int N, M;
-  static int[][] board;
-  static int[] dx = {1, 0, -1, 0};
-  static int[] dy = {0, 1, 0, -1};
-  static int ans = Integer.MAX_VALUE;
-  static int bfs(){
-    Deque<int[]> q = new ArrayDeque<>();
-    q.add(new int[]{0, 0, 0});
-    int[][][] visited = new int[N][M][2];
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < M; j++) {
-        visited[i][j][0] = 0;
-      }
-    }
-    visited[0][0][0] = 1;
-    while(!q.isEmpty()){
-      int[] coords = q.pop();
-      int x = coords[0], y = coords[1], z = coords[2];
-      if(x == N - 1 && y == M - 1){
-        return visited[x][y][z];
-      }
-      for(int i = 0 ; i < 4 ; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(0 <= nx && nx < N && 0 <= ny && ny < M){
-          if(board[nx][ny] == 1 && z == 0){
-            visited[nx][ny][1] = visited[x][y][0] + 1;
-            q.add(new int[]{nx, ny, 1});
-          }else if(board[nx][ny] == 0 && visited[nx][ny][z] == 0){
-            visited[nx][ny][z] = visited[x][y][z] + 1;
-            q.add(new int[]{nx, ny, z});
-          }
-        }
-      }
-    }
-    return -1;
-  }
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int[] inputs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-    N = inputs[0];
-    M = inputs[1];
-    board = new int[N][M];
-    for(int i = 0 ; i < N ; i++) {
-      board[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
-    }
-    System.out.println(bfs());
-  }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int[] dx = {1, 0, -1, 0};
+	static int[] dy = {0, 1, 0, -1};
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] map = new int[N + 1][M + 1];
+		for(int i = 1 ; i < N + 1 ; i++) {
+			String[] tmp = br.readLine().split("");
+			for(int j = 1 ; j < M + 1 ; j++) {
+				map[i][j] = Integer.parseInt(tmp[j - 1]);
+			}
+		}
+		boolean[][][] v = new boolean[N + 1][M + 1][2];
+		
+		Deque<int[]> q = new ArrayDeque<>();
+		q.offer(new int[] {1, 1, 0, 1});
+		v[1][1][0] = true;
+		int answer = -1;
+		while(!q.isEmpty()) {
+			int[] coord = q.poll();
+			int x = coord[0];
+			int y = coord[1];
+			if(x == N && y == M) {
+				answer = coord[3];
+				break;
+			}
+			
+			for(int i = 0 ; i < 4 ; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if(0 < nx && nx <= N && 0 < ny && ny <= M) {
+					if(map[nx][ny] == 0 && !v[nx][ny][coord[2]]) {
+						v[nx][ny][coord[2]] = true;
+						q.offer(new int[] {nx, ny, coord[2], coord[3] + 1});
+					}else if(map[nx][ny] == 1) {
+						if(coord[2] == 0) {
+							v[nx][ny][1] = true;
+							q.offer(new int[] {nx, ny, 1, coord[3] + 1});
+						}
+					}
+				}
+			}
+			
+		}
+		bw.write(answer + "");
+		bw.close();
+		
+		
+	}
 }
