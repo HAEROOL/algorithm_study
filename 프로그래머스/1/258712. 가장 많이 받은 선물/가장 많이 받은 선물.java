29dacd7b84f08340590d7f868450c3dd84f3c700
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2025-10-07 21:15:24 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 20.21 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -3,52 +3,73 @@ import java.util.*;
 class Solution {
     public int solution(String[] friends, String[] gifts) {
         int answer = 0;
-        Map<String,Map<String, Integer>> map = new HashMap<>();
+        Map<String, Map<String,Integer>> map = new HashMap<>();
         Map<String, Integer> ratioMap = new HashMap<>();
-        for(int i = 0 ; i < friends.length ; i++){
+        Map<String, Integer> answerMap = new HashMap<>();
+        for(String f : friends){
             Map<String, Integer> fmap = new HashMap<>();
-            
-            for(int j = 0 ; j < friends.length ; j++){
-                if(i == j) continue;
-                fmap.put(friends[j], 0);
+            for(int i = 0 ; i < friends.length ; i++){
+                if(!friends[i].equals(f)){
+                    fmap.put(friends[i], 0);
+                }
             }
-            map.put(friends[i], fmap);
-            ratioMap.put(friends[i], 0);
+            map.put(f, fmap);
+            ratioMap.put(f, 0);
+            answerMap.put(f, 0);
         }
         
-        for(int i = 0 ; i < gifts.length ; i++){
-            String[] tmp = gifts[i].split(" ");
+        for(String g : gifts){
+            String[] tmp = g.split(" ");
             String giver = tmp[0];
             String reciever = tmp[1];
             
-            map.get(reciever).replace(giver, map.get(reciever).get(giver) + 1);
-            
-            ratioMap.replace(giver, ratioMap.get(giver) + 1);
-            ratioMap.replace(reciever, ratioMap.get(reciever) - 1);
+            Map<String, Integer> giftMap = map.get(giver);
+            if(giftMap.get(reciever) != null){
+                giftMap.replace(reciever, giftMap.get(reciever) + 1);
+            }else{
+                giftMap.put(reciever, 1);
+            }
         }
-        for(String key : ratioMap.keySet()){
-            System.out.println(key + " " + ratioMap.get(key));
-        }
-        for(int i = 0 ; i < friends.length ; i++){
-            int total = 0;
-            String rname = friends[i];
-            Map<String, Integer> fmap = map.get(rname);
-            
-            for(String name : fmap.keySet()){
-                int rnum = map.get(name).get(rname);
-                if(rnum > map.get(rname).get(name)){
-                    total++;
-                }else if(rnum == map.get(rname).get(name)){
-                    int rratio = ratioMap.get(rname);
-                    int gratio = ratioMap.get(name);
-                    
-                    if(gratio < rratio) total++;
+        
+        for(String giver : map.keySet()){
+            int giveCnt = 0;
+            int recieveCnt = 0;
+            for(String reciever : map.get(giver).keySet()){
+                giveCnt += map.get(giver).get(reciever);
+                if(map.get(reciever).get(giver) != null){
+                    recieveCnt += map.get(reciever).get(giver);
                 }
             }
-            answer = Math.max(total, answer);
+            ratioMap.replace(giver, giveCnt - recieveCnt);
         }
         
-        
+        for(int i = 0 ; i < friends.length ; i++){
+            for(int j = i + 1 ; j < friends.length ; j++){
+                String A = friends[i];
+                String B = friends[j];
+                int Acnt = map.get(A).get(B);
+                int Bcnt = map.get(B).get(A);
+                
+                if(Acnt > Bcnt){
+                    answerMap.replace(A, answerMap.get(A) + 1);
+                }else if(Bcnt > Acnt){
+                    answerMap.replace(B, answerMap.get(B) + 1);
+                }else{
+                    int Aratio = ratioMap.get(A);
+                    int Bratio = ratioMap.get(B);
+                    if(Aratio > Bratio){
+                        answerMap.replace(A, answerMap.get(A) + 1);
+                    }else if(Bratio > Aratio){
+                        answerMap.replace(B, answerMap.get(B) + 1);
+                    }
+                }
+            }
+        }
+        for(String name : answerMap.keySet()){
+            if(answer < answerMap.get(name)){
+                answer = answerMap.get(name);
+            }
+        }
         return answer;
     }
 }
