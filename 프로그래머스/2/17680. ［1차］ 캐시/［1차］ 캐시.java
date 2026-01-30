d9b4da64af0a30cc0ff24bf37f1dbb12623ee337
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2026-01-30 21:12:54 +0900

    [level 2] Title: [1차] 캐시, Time: 38.58 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,45 +3,29 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        int HIT = 1;
-        int MISS = 5;
-        Map<String, Integer> timeMap = new HashMap<String, Integer>();
-        Set<String> cacheSet = new HashSet<String>();
+        List<String> cache = new ArrayList<>();
         
+        // 도시를 입력받는다
         for(String city : cities){
-            timeMap.put(city.toLowerCase(), Integer.MAX_VALUE);
-        }
-        if(cacheSize == 0) return cities.length * MISS;
-        for(int i = 0 ; i < cities.length ; i++){
-            String city = cities[i].toLowerCase();
-            // System.out.println(cacheSet);
-            if(cacheSet.contains(city)){
-                answer += HIT;
-                timeMap.replace(city, i);
-                // System.out.println("HIT: " + city);
-                // System.out.println(timeMap);
+            // 캐시에 없을 경우(+ 5)
+            String c = city.toLowerCase();
+            if(!cache.contains(c)){
+                // 캐시에 추가한다.
+                cache.add(0, c);
+                answer += 5;
             }else{
-                if(cacheSet.size() == cacheSize){
-                    int time = i;
-                    String rcity = "";
-                    for(String key : timeMap.keySet()){
-                        if(time > timeMap.get(key) && cacheSet.contains(key)){
-                            rcity = key;
-                            time = timeMap.get(key);
-                        }
-                    }
-                    cacheSet.remove(rcity);
-                    answer += MISS;
-                    cacheSet.add(city);
-                    timeMap.replace(city, i);
-                }else{
-                    answer += MISS;
-                    cacheSet.add(city);
-                    timeMap.replace(city, i);
-                }
+                // 캐시에 있을 경우(+ 1)
+                // 캐시에서 빼서 다시 앞으로 둔다
+                cache.remove(c);
+                cache.add(0, c);
+                answer += 1;
             }
-            
-        }
+            // 캐시에 들어간 값이 cacheZise 보다 크면 마지막 요소를 없앤다.            
+            if(cache.size() > cacheSize){
+                cache.remove(cache.size() - 1);
+            }
+        } 
+
         return answer;
     }
 }
