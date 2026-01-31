db4f9b1d332487efccc961eef3a86d70d6bac8cf
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2026-01-31 21:15:59 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 12.53 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -3,30 +3,36 @@ import java.util.*;
 class Solution {
     public int solution(String str1, String str2) {
         List<String> A = new ArrayList<>();
-        for(int i = 0 ; i < str1.length() - 1 ; i++){
-            String str = str1.substring(i, i+2);
-            if(str.matches("[a-zA-Z]{2}"))
-                A.add(str.toLowerCase());
-        }
-
         List<String> B = new ArrayList<>();
-        for(int i = 0 ; i < str2.length() - 1 ; i++){
-            String str = str2.substring(i, i+2);
-            if(str.matches("[a-zA-Z]{2}"))
-                B.add(str.toLowerCase());
-        }
-        int inter = 0;
-        for(String e : A){
-            if(B.contains(e)){
-                inter++;
-                B.remove(e);
+        
+        for(int i = 0 ; i < str1.length() - 1 ; i++){
+            String str = str1.substring(i, i + 2);
+            if(str.matches("[a-zA-Z]{2}")){
+                A.add(str.toLowerCase());
             }
         }
+        for(int i = 0 ; i < str2.length() - 1 ; i++){
+            String str = str2.substring(i, i + 2);
+            if(str.matches("[a-zA-Z]{2}")){
+                B.add(str.toLowerCase());
+            }
+        }
+        
+        int inter = 0;
+        for(int i = 0 ; i < A.size() ; i++){
+            String s = A.get(i);
+            if(B.contains(s)){
+                B.remove(s);
+                inter++;
+            }
+        }
+        
         int union = A.size() + B.size();
+        
         if(A.size() == 0 && B.size() == 0) return 65536;
         if(inter == union) return 65536;
-        // System.out.println();
         float answer = (float)inter / (float)union * 65536;
+        
         return (int)answer;
     }
 }
