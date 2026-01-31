975e9237b653df742c057aea7b2bd38a01eb43b9
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2026-01-31 22:58:29 +0900

    [level 2] Title: [3차] 압축, Time: 13.53 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -2,32 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String msg) {
-        List<Integer> answer = new ArrayList<>();;
-        Map<String, Integer> dict = new HashMap<>();
+        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
         for(int i = 1 ; i < 27 ; i++){
             int code = 64 + i;
             String ascii = String.valueOf((char)code);
-            dict.put(ascii, i);
+            map.put(ascii, i);
         }
-        String[] strs = msg.split("");
-        String w = strs[0];
+        
+        String[] arr = msg.split("");
+        
+        int pos = 1;
         int id = 27;
-        for(int i = 1 ; i < strs.length ; i++ ){
-            String c = strs[i];
-            String wc = w + c;
-            if(dict.containsKey(wc)){
-                w = wc;
+        String token = arr[0];
+        while(pos < arr.length){
+            if(map.containsKey(token + arr[pos])){
+                token += arr[pos];    
             }else{
-                answer.add(dict.get(w));
-                dict.put(wc, id++);
-                w = c;
+                list.add(map.get(token));
+                map.put(token + arr[pos], id++);
+                token = arr[pos];
             }
+            pos++;
         }
-        answer.add(dict.get(w));
-        int[] ans = new int[answer.size()];
-        for(int i = 0 ; i < ans.length ; i++){
-            ans[i] = answer.get(i);
-        }
-        return ans;
+        list.add(map.get(token));
+        
+        answer = new int[list.size()];
+        for(int i = 0 ; i < answer.length ; i++) answer[i] = list.get(i);
+        return answer;
     }
 }
