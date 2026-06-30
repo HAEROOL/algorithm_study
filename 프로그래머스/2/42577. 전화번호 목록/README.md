9df627ba05fe90f97e965e687f017d6d7fff9b77
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2026-06-30 20:59:53 +0900

    [level 2] Title: 전화번호 목록, Time: 186.73 ms, Memory: 231 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
 
 ### 성능 요약
 
-메모리: 28.1 MB, 시간: 81.98 ms
+메모리: 231 MB, 시간: 186.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 5일 16:28:21
+2026년 06월 30일 20:59:50
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashMap<String, Boolean> map = new HashMap<>();
+        for(String phone_num : phone_book) {
+            map.putIfAbsent(phone_num, true);
+        }
+        
+        for(int i = 0 ; i < phone_book.length ; i++){
+            if(phone_book[i].length() == 1) continue;
+            for(int j = 1 ; j < phone_book[i].length() ; j++){
+                if(map.getOrDefault(phone_book[i].substring(0,j), false)){
+                    return false;   
+                }    
+            }
+        }
+        
+        return answer;
+    }
+}
