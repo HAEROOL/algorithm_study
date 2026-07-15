195a786764ce20d93a3fe64a7932410133e4701c
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2026-07-15 21:23:38 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 36.49 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -3,39 +3,40 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] gems) {
         int[] answer = {};
+        Set<String> gemSet = new HashSet<>();
         Map<String, Integer> map = new HashMap<>();
-        Set<String> set = new HashSet<>();
-        Set<String> ansSet = new HashSet<>();
-        for(String g : gems) ansSet.add(g);
-        int tgt = ansSet.size();
-        int start = 0;
-        int end = 0;
-        int size = Integer.MAX_VALUE;
-        while(start <= end){
-            if(set.size() < tgt && end < gems.length){
-                set.add(gems[end]);
-                if(map.containsKey(gems[end])) map.replace(gems[end], map.get(gems[end]) + 1);
-                else map.put(gems[end], 1);
-                end++;
-                // System.out.println(set + " " + start + " " + end);
-            }else{
-                if(start == gems.length) break;
-                if(set.size() == tgt && end - start + 1 < size){
-                    answer = new int[]{start + 1, end};
-                    size = end - start + 1;
-                }
-                String gem = gems[start];
-                int cnt = map.get(gem);
-                if(cnt == 1) {
-                    set.remove(gem);
-                }
-                map.replace(gem, cnt - 1);
-                start++;
-
+        
+        for(int i = 0 ; i < gems.length ; i++){
+            gemSet.add(gems[i]);
+            map.putIfAbsent(gems[i], 0);
+        }
+        int left = 0;
+        int right = 0;
+        int total = 1;
+        map.put(gems[0], 1);
+        int mx = Integer.MAX_VALUE;
+        while(left <= right){
+            if(right == gems.length || left == gems.length) break;
+            
+            if(total < gemSet.size()){
+                right++;
+                if(right == gems.length) break;
+                map.put(gems[right], map.get(gems[right]) + 1);
+                if(map.get(gems[right]) == 1) total++;
                 
-                // System.out.println(set + " " + start + " " + end);
+            }else if(total == gemSet.size()){
+                if(mx > right - left){
+                    mx = right - left;
+                    answer = new int[]{left + 1, right + 1};
+                }
+                map.put(gems[left], map.get(gems[left]) - 1);
+                if(map.get(gems[left]) == 0) total--;
+                
+                left++;
             }
         }
+        
+        
         return answer;
     }
 }
