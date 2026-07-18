857fd91167596acdbcf856ad72277a74d14fc2d7
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2026-07-18 16:09:51 +0900

    [level 3] Title: 아이템 줍기, Time: 0.50 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,53 +1,79 @@
 import java.util.*;
-
 class Solution {
-    static int answer;
-    static int[][] map;
-    static int[] dx = new int[]{1, 0, -1, 0};
-    static int[] dy = new int[]{0, 1, 0, -1};
-    static void drawMap(int lx, int ly, int rx, int ry){
-        for(int i = lx ; i < rx + 1 ; i++){
-            for(int j = ly ; j < ry + 1 ; j++){
-                if((i == lx || i == rx || j == ly || j == ry) && map[i][j] != 2) map[i][j] = 1;
-                else map[i][j] = 2;
+    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        int answer = 0;
+        int[] dx = new int[]{0, 1, 0, -1};
+        int[] dy = new int[]{1, 0, -1, 0};
+        int[][] map = new int[101][101];
+        
+        for(int[] coord : rectangle){
+            int sy = coord[0] * 2;
+            int sx = coord[1] * 2;
+            int ey = coord[2] * 2;
+            int ex = coord[3] * 2;
+            
+            for(int i = sx ; i < ex ; i++){
+                map[i][sy] = 1;
+            }
+            for(int i = sx ; i < ex ; i++){
+                map[i][ey] = 1;
+            }
+            for(int i = sy ; i <= ey ; i++){
+                map[sx][i] = 1;
+            }
+            for(int i = sy ; i <= ey ; i++){
+                map[ex][i] = 1;
             }
         }
-    }
-    static int bfs(int sx, int sy, int tx, int ty){
-        boolean[][] v = new boolean[101][101];
-        Deque<int[]> q = new ArrayDeque<int[]>();
-        q.offer(new int[]{sx, sy, 0});
-        v[sx][sy] = true;
-        while(!q.isEmpty()){
-            int[] coord = q.poll();
-            int x = coord[0];
-            int y = coord[1];
-            if(x == tx && y == ty) {
-                return coord[2] / 2;
-            }
-            for(int i = 0 ; i < 4 ; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if(0 <= nx && nx < 101 && 0 <= ny && ny < 101 && !v[nx][ny] && map[nx][ny] == 1){
-                    v[nx][ny] = true;
-                    q.offer(new int[]{nx, ny, coord[2] + 1});
+        
+        for(int[] coord : rectangle){
+            int sy = coord[0] * 2;
+            int sx = coord[1] * 2;
+            int ey = coord[2] * 2;
+            int ex = coord[3] * 2;
+            
+            for(int i = sx + 1 ; i < ex ; i++){
+                for(int j = sy + 1 ; j < ey ; j++){
+                    map[i][j] = 0;
                 }
             }
         }
-        return 0;
         
-    }
-    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        answer = 0;
-        map = new int[101][101];
-        for(int[] coord : rectangle){
-            int ly = coord[0];
-            int lx = coord[1];
-            int ry = coord[2];
-            int rx = coord[3];
-            drawMap(lx * 2, ly * 2, rx * 2, ry * 2);
+//         for(int[] row : map){
+//             System.out.println(Arrays.toString(row));
+//         }
+        
+        int cy = characterX * 2;
+        int cx = characterY * 2;
+        
+        int ty = itemX * 2;
+        int tx = itemY * 2;
+        
+        int[][] v = new int[101][101];
+        Deque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{cx, cy});
+        v[cx][cy] = 1;
+        
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+            
+            if(now[0] == tx && now[1] == ty){
+                answer = (v[now[0]][now[1]] - 1) / 2;
+                break;
+            }
+            
+            for(int i = 0 ; i < 4 ; i++){
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+                
+                if(0 <= nx && nx < 101 && 0 <= ny && ny < 101 && v[nx][ny] == 0 && map[nx][ny] == 1){
+                    q.offer(new int[]{nx, ny});
+                    v[nx][ny] = v[now[0]][now[1]] + 1;
+                }
+            }
         }
-        answer = bfs(characterY * 2, characterX * 2, itemY * 2, itemX * 2);
+        
+        
         return answer;
     }
 }
