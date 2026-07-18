f59760b2ec1c9c161f9265309c6ffcab0fc8519e
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2026-07-18 16:34:34 +0900

    [level 3] Title: 가장 먼 노드, Time: 21.23 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,60 +1,62 @@
 import java.util.*;
-
 class Solution {
     static List<Integer>[] map;
     static int N;
-    static int answer = 0;
-    static void dijkstra(int start){
+    static int dijkstra(int start){
+        boolean[] v = new boolean[N + 1];
         int[] dist = new int[N + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        boolean[] v = new boolean[N + 1];
         
-        dist[start] = 0;
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> {
+        PriorityQueue<int[]> q = new PriorityQueue<int[]>((a, b) -> {
             return a[1] - b[1];
         });
-        pq.offer(new int[]{start, 0});
-        while(!pq.isEmpty()){
-            int[] n = pq.poll();
-            int now = n[0];
+        
+        q.offer(new int[]{start, 0});
+        dist[start] = 0;
+        
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+            if(v[now[0]]) continue;
+            v[now[0]] = true;
             
-            if(v[now]) continue;
-            v[now] = true;
-            for(int next : map[now]){
-                int nextNode = next;
-                int nextCost = 1;
-                if(dist[nextNode] > dist[now] + nextCost){
-                    pq.offer(new int[]{nextNode, dist[now] + 1});
-                    dist[nextNode] = dist[now] + 1;
+            for(int next : map[now[0]]){
+                if(dist[next] > now[1] + 1){
+                    dist[next] = now[1] + 1;
+                    q.offer(new int[]{next, dist[next]});
                 }
             }
         }
+        
         int mx = -1;
-        for(int i : dist){
-            if(i == Integer.MAX_VALUE) continue;
-            mx = Math.max(i, mx);
+        for(int i = 1 ; i < N + 1; i++){
+            if(dist[i] == Integer.MAX_VALUE) continue;
+            mx = Math.max(mx, dist[i]);
         }
+        
+        int res = 0;
         for(int i : dist){
-            if(i == mx) answer++;
+            if(mx == i) res++;
         }
-         
+        // System.out.println(Arrays.toString(dist));
+        return res;
     }
     public int solution(int n, int[][] edge) {
-        answer = 0;
-        N = n;
+        int answer = 0;
         map = new ArrayList[n + 1];
-        for(int i = 1 ; i < n + 1 ; i++){
-            map[i] = new ArrayList<Integer>();
+        N = n;
+        
+        for(int i = 0 ; i < N + 1 ; i++){
+            map[i] = new ArrayList<>();
+        }
+        for(int[] tmp : edge){
+            int from = tmp[0];
+            int to = tmp[1];
+            map[from].add(to);
+            map[to].add(from);
         }
         
-        for(int[] e : edge){
-            int a = e[0];
-            int b = e[1];
-            map[a].add(b);
-            map[b].add(a);
-        }
+        answer = dijkstra(1);
         
-        dijkstra(1);
         return answer;
     }
 }
